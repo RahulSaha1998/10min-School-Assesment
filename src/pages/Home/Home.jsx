b7292fd94c4a1d2a47a8476fd6923c29b969769c
RahulSaha1998 <rahulsaha87928@gmail.com>
2025-07-24 23:16:59 +0600

    Load data in home from the api

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [course, setCourse] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.10minuteschool.com/discovery-service/api/v1/products/ielts-course?lang=en', {
+            headers: {
+                'X-TENMS-SOURCE-PLATFORM': 'web',
+                'accept': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => setCourse(data?.data));
+    }, []);
+
+     if (!course) {
+        return <p>Loading...</p>;
+    }
+
+    console.log(course);
+
+    // const video = course.media?.find(m => m.type === 'youtube');
+    // const instructorSection = course.sections?.find(s => s.type === 'instructor');
+
     return (
         <div>
-            <h2>Hello from Home</h2>
+            <h2>HEllo bro</h2>
         </div>
     );
 };
